ca02d8aaecc68a574829c117405959947be26af9
Nury-Fatema <nury.fatema082@gmail.com>
2020-04-06 22:14:19 +0600

    first commit

---
+++ src/Auth/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb-GqxeO9-0Ej8otE7JqUQ3AkolJDMY2c",
+    authDomain: "hot-onion-restora-ef669.firebaseapp.com",
+    databaseURL: "https://hot-onion-restora-ef669.firebaseio.com",
+    projectId: "hot-onion-restora-ef669",
+    storageBucket: "hot-onion-restora-ef669.appspot.com",
+    messagingSenderId: "1057483548280",
+    appId: "1:1057483548280:web:86197cc1d957d016406f8d"
+  };
+  export default firebaseConfig;
